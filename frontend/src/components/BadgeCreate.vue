107d031f1254b94c3f33bc7562751f4bb2e29e92
ciamicor <162640077+ciamicor@users.noreply.github.com>
2025-09-29 12:05:52 -0700

    slightly updated badge form UX
    now better works as public-facing form.

--- frontend/src/components/BadgeCreate.vue
+++ frontend/src/components/BadgeCreate.vue
@@ -2,11 +2,15 @@
 
   <div class="row-12-300 --gap-24 --place-content-center --p-4">
     <form
-      class="col-12-300 col-10-600 col-8-900 --p-b-24 --p-t-12"
+      v-show="!showQr"
+      class="col-12-300 col-10-600 col-8-900 --p-b-0 --p-t-12"
       @submit.prevent="createAttendee(attendee)"
     >
-      <h4 class="--m-0">{{ expoLocalData.expo_Client }} {{ expoLocalData.expo_Year }} Supplier's
-                        Day</h4>
+      <h4 class="--m-0">
+        {{expoLocalData.expo_Client}}
+        {{expoLocalData.expo_Year}}
+        Supplier's Day
+      </h4>
       <h1 id="attendee-reg">Attendee Registration</h1>
       <p>All fields are required.</p>
       <div class="row-12-300 --items">
@@ -18,7 +22,6 @@
             placeholder="First Name"
             required
             type="text">
-
         </label>
         <label>
           Last Name
@@ -28,7 +31,6 @@
             placeholder="Last Name"
             required
             type="text">
-
         </label>
       </div>
       <div class="row-12-300 --items">
@@ -40,7 +42,6 @@
             placeholder="Employer"
             required
             type="text"></label>
-
         <label>
           Position/Title
           <input
@@ -61,7 +62,6 @@
             placeholder="Email Address"
             required
             type="email"/>
-
         </label>
         <label>
           Phone Number
@@ -71,7 +71,6 @@
             placeholder="Phone Number"
             required
             type="tel"/>
-
         </label>
       </div>
       <div class="row-12-300 --items">
@@ -83,7 +82,6 @@
             placeholder="Enter your address"
             required
             type="text"/>
-
         </label>
       </div>
       <label>
@@ -108,7 +106,6 @@
                 <option value="">No</option>
               </select>
             </label>-->
-
       <button v-if="!showQr"
               class="--success --m-t-12"
               type="submit"
@@ -116,17 +113,23 @@
         Submit Registration
       </button>
     </form>
-    <div class="row-12-300 --justify-content-center --align-content-start">
-      <div class="col-12-300 col-10-600 col-8-900">
-        <button
-          v-if="showQr"
-          class="--secondary"
-          @click="printBadge_Portrait3x4(attendee)">
-          Print {{ attendee.name_First }}'s Badge
-        </button>
-      </div>
+    <div v-show="showQr"
+         class="row-10-300 --justify-content-center --align-content-start">
+      <p class="--flex-basis-100 --place-self-center">
+        Thanks for registering for the {{expoLocalData.expo_Year}}
+        {{expoLocalData.expo_Client}} Suppliers' Day, {{attendee.name_First}}.
+      </p>
+      <button
+        class="--secondary --flex-grow"
+        @click="printBadge_Portrait3x4(attendee)">
+        Print {{attendee.name_First}}'s Badge
+      </button>
+      <button
+        class="--success --flex-grow"
+        @click="resetForm">
+        Register Another Badge
+      </button>
     </div>
-
     <div class="badges-page-container">
       <img id="badge-logo"
            :alt="`${companyLocalData.name}-logo`"
@@ -160,7 +163,8 @@ const companyLocalData = useCompanyLocalStore()
 const expoLocalData = useExpoLocalStore()
 
 function getImageUrl( name ) {
-  return new URL( `../../public/logos/${ expoLocalData.expo_Client.toString().toLowerCase() }/${ name }.jpeg`, import.meta.url ).href
+  return new URL( `../../public/logos/${ expoLocalData.expo_Client.toString()
+    .toLowerCase() }/${ name }.jpeg`, import.meta.url ).href
 }
 
 /*-| Print Component |-*/
@@ -187,6 +191,23 @@ async function createAttendee( a ) {
   attendeeId.value = attendee.value.id.toString()
   console.log( 'New Attendee\'s ID is: ', typeof attendeeId.value, attendeeId.value )
   showQr.value = true
+}
+
+function resetForm() {
+  showQr.value = false
+  attendee.value = {
+    expo_Year: expoLocalData.expo_Year,
+    expo_Client: expoLocalData.expo_Client,
+    name_First: '',
+    name_Last: '',
+    contact_Email: '',
+    contact_Phone: '',
+    contact_Employer: '',
+    address: '',
+    title: '',
+    reg_Type: '',
+    tech_Sem: ''
+  }
 }
 
 /*-| Printing |-*/
@@ -230,14 +251,33 @@ async function printBadge_Portrait3x4( a ) {
   badgePdf.setFontSize( 18 )
   badgePdf.text( a.contact_Employer, dim.p * 2, dim.w - dim.p, null, dim.rot )
   badgePdf.setFontSize( 22 )
-  badgePdf.text( toTitleCase_Service( `${ a.name_First } ${ a.name_Last }` ), dim.p * 4, dim.w - dim.p, dim.rot )
+  badgePdf.text( toTitleCase_Service( `${ a.name_First } ${ a.name_Last }` ),
+    dim.p * 4,
+    dim.w - dim.p,
+    dim.rot )
   badgePdf.setFontSize( 18 )
   badgePdf.text( toTitleCase_Service( a.title ), dim.p * 6, dim.w - dim.p, dim.rot )
 
   /*-| Add QR Code |-*/
-  badgePdf.addImage( qrData.value, 'PNG', dim.h - dim.p, dim.w - dim.imgH - dim.p, dim.imgH, dim.imgH, 'qr', 'FAST', dim.rot )
+  badgePdf.addImage( qrData.value,
+    'PNG',
+    dim.h - dim.p,
+    dim.w - dim.imgH - dim.p,
+    dim.imgH,
+    dim.imgH,
+    'qr',
+    'FAST',
+    dim.rot )
   /*-| Add Logo |-*/
-  badgePdf.addImage( qrLogo.value, 'PNG', dim.h - dim.p, dim.p * 4, dim.imgW, dim.imgH, 'logo', 'FAST', dim.rot )
+  badgePdf.addImage( qrLogo.value,
+    'PNG',
+    dim.h - dim.p,
+    dim.p * 4,
+    dim.imgW,
+    dim.imgH,
+    'logo',
+    'FAST',
+    dim.rot )
 
   setTimeout( () => {
     badgePdf.output( 'dataurlnewwindow' )
